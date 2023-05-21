d19f59efe0e8f33bd8d6b967a6f5fcd6027e9192
Pau Olives <pauof25@gmail.com>
2023-05-21 19:15:56 +0200

    environments

--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'http://10.34.17.210:3000'
-}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://10.34.17.20:3000'
+  // production: false,
+  apiUrl: ''
 }
